e695bd553f64a7a8e0331d993dbe6aff8c2e8b4d
arun02072002 <147825943+arun02072002@users.noreply.github.com>
2023-11-16 12:57:46 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace application
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            // implicit casting //
+            int myInt = 10;
+            double mydouble = myInt;
+
+            Console.WriteLine("my int is :"+ myInt);
+            Console.WriteLine("my double is :"+mydouble);
+
+            // explicit casting //
+
+            double myDouble = 9.55;
+            int myint = (int)mydouble;
+            Console.WriteLine("my int is :"+myint);
+            Console.WriteLine("my double is "+myDouble);
+
+            int mInt = 15;
+            double mdouble = 5.55;
+            bool mybool = false;
+
+            Console.WriteLine(Convert.ToString(mInt));
+            Console.WriteLine(Convert.ToDouble(mInt));
+            Console.WriteLine(Convert.ToInt32(mdouble));
+            Console.WriteLine(Convert.ToString(mybool));
+
+        }
+    }
+}
